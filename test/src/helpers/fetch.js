f690feb8fd7fa3a65eefd28b94f16b9ee25d9f83
Tim Alby <timothee.alby@gmail.com>
2020-04-19 17:46:52 +0200

    refactor fetch methods

--- test/src/helpers/fetch.js
+++ test/src/helpers/fetch.js
@@ -11,25 +11,27 @@ function makeJwt(userId) {
   )
 }
 
-module.exports = {
-  get: async (path, userId) => {
-    const response = await fetch(`${process.env.PGRST_API_URL}${path}`, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${makeJwt(userId)}`
-      }
-    })
-    return response.json()
-  },
+async function request(method, path, userId, body) {
+  return fetch(`${process.env.PGRST_API_URL}${path}`, {
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${makeJwt(userId)}`
+    },
+    body
+  })
+}
 
-  post: async (path, userId, json) => {
-    return fetch(`${process.env.PGRST_API_URL}${path}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${makeJwt(userId)}`
-      },
-      body: JSON.stringify(json)
-    })
-  }
+async function get(path, userId) {
+  const response = await request('GET', path, userId)
+  return response.json()
+}
+
+async function post(path, userId, json) {
+  return request('POST', path, userId, JSON.stringify(json))
+}
+
+module.exports = {
+  get,
+  post
 }
