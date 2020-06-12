b26b92095a860f261365ba59ce9d6242ab12b96a
Tim Alby <timothee.alby@gmail.com>
2020-06-12 16:01:19 +0200

    [client] fix rooms hero test style

--- client/components/rooms/list-empty.jsx
+++ client/components/rooms/list-empty.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import { Box, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
 import RoomCreate from '~/components/rooms/create'
 import { useTranslation } from 'react-i18next'
 
+const useStyles = makeStyles(theme => ({
+  heroText: {
+    color: theme.palette.common.white,
+    textAlign: 'center'
+  }
+}))
+
 const RoomsList = () => {
   const { t } = useTranslation()
+  const classes = useStyles()
   return (
     <>
-      <Box display="flex" justifyContent="center" paddingTop={4}>
+      <Box
+        display="flex"
+        justifyContent="center"
+        paddingTop={4}
+        className={classes.heroText}
+      >
         <Typography variant="h5" component="h2">
           {t('no rooms yet')}
         </Typography>
